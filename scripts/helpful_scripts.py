7dcae27f9e0c6250391cb7e542e15dc4dbf217f8
Abel De Jesus <adejesus@contractor.appsbees.com>
2022-03-18 15:27:05 -0400

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,15 @@
+from brownie import accounts, network, config
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development', 'ganache-local', 'mainnet-fork', 'ganache']
+
+def get_account(index=None, id=None):
+
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+   
+    return accounts.add(config['wallets']['from_key'])
